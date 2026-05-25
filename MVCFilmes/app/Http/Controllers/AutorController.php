0fcf7af1d93a9165a51160dbc7029ab147094ab5
Bruna <bruna.f.silva22@aluno.senai.br>
2026-05-25 07:56:11 -0300

    Atividade MVC Filmes

--- MVCFilmes/app/Http/Controllers/AutorController.php
+++ MVCFilmes/app/Http/Controllers/AutorController.php
@@ -64,7 +64,7 @@ class AutorController extends Controller
    public function deletar($id){
         $autor = Autor::findOrFail($id); // buscar o autor para depois deletar
         $autor->delete(); // faz o delete no banco de dados
-        $autor->deletar();
+        $autor->delete();
 
         return redirect()->route('autor.listar')
             ->with('success','Autor excluído com sucesso!');
